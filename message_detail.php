f70688f475408292fa77c9aaeaabf54d475ad5fa
Andrew Anderson <Anderson7301@icloud.com>
2015-04-26 11:32:46 -0400

    detail

---
+++ message_detail.php
@@ -0,0 +1,23 @@
+<?php  include('header.php') ?>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+      <div style="text-align: center; margin-top: 5px">
+        <form role="form">
+          <div class="form-group">
+            <label for="name"></label>
+            <textarea class="form-control" rows="3"></textarea>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<?php  include('footer.php') ?>
+
+
+
+
