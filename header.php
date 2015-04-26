bc993bab1249411334a48011a244be9ac492c1e3
Ankur Saxena <saxenaankur001@gmail.com>
2015-04-26 05:22:38 -0400

    header stuff

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Flat UI - Free Bootstrap Framework and Theme</title>
-    <meta name="description" content="Flat UI Kit Free is a Twitter Bootstrap Framework design and Theme, this responsive framework includes a PSD and HTML version."/>
+    <title>Let's Go Have a Spontaneous Adventure!</title>
+    <meta name="description" content="Spontaneous Adventure App"/>
 
     <meta name="viewport" content="width=1000, initial-scale=1.0, maximum-scale=1.0">
 
@@ -12,6 +12,7 @@
 
     <!-- Loading Flat UI -->
     <link href="dist/css/flat-ui.css" rel="stylesheet">
+    <link href="bower_components/fontawesome/css/font-awesome.css" rel="stylesheet">
     <link href="docs/assets/css/demo.css" rel="stylesheet">
 
     <link rel="shortcut icon" href="img/favicon.ico">
@@ -23,3 +24,78 @@
     <![endif]-->
   </head>
   <body>
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Let's Go!</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a class="btn btn-primary active" href="#fakelink"><span class="fui-heart"></span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+  <div class="row">
+    <div class="col-xs-3">
+      <a class="navbar-brand" href="match.php">Let's Go!</a>
+    </div>
+
+    <div class="col-xs-3">
+      <a class="navbar-brand" href="messages.php"><span class="fui-mail"></span>
+    </div>
+
+    <div class="col-xs-3">
+      <a class="navbar-brand" href="messages.php"><span class="fui-heart"></span>
+    </div>
+
+    <div class="col-xs-2">
+      <a class="navbar-brand" href="messages.php"><span class="fui-gear"></span>
+    </div>
+  </div>
+
+
+<?php include('footer.php'); ?>
