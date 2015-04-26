8cfa2f0c40a32ee8bf4e882c29d9618e0dac5f1f
Andrew Anderson <Anderson7301@icloud.com>
2015-04-26 10:51:46 -0400

    confirmation

---
+++ confirmation.php
@@ -0,0 +1,20 @@
+<?php  include('header.php') ?>
+
+
+<div class="container-fluid">
+  <h4 style="text-align: center;">You Have A Match!</h4>
+  <div class="row">
+    <div class="col-xs-12 center">
+        <div class="thumbnail">
+          <img src="./img/heart.png" width="400" height="400" />
+          <div class="caption">
+            <a class="btn btn-block btn-md btn-info"  style="background-color:#1abc9c;">
+              <i class="facebook"></i> Plan the trip
+            </a>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+<?php  include('footer.php') ?>
