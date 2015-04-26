40664924ac1b1f3e82d153130c9abd3d32c30546
Andrew Anderson <Anderson7301@icloud.com>
2015-04-26 04:25:33 -0400

    signin signup

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php include 'header.php' ?>
+
+     
+<div class="container">
+  <h3 class="demo-panel-title">Sign In</h3>
+  <div class="row">
+    <div class="col-xs-12 center">
+      <div class="form-group">
+        <input type="text" value="" placeholder="Email" class="form-control" />
+      </div>
+      <div class="form-group">
+        <input type="text" value="" placeholder="Passowrd" class="form-control" />
+      </div>
+      <div class="">
+      <a class="btn btn-block btn-lg btn-info" onclick="_gaq.push(['_trackEvent', 'btn-social', 'click', 'btn-facebook']);">
+        <i class="fa fa-facebook"></i> Sign in with Facebook
+      </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include 'footer.php' ?>
+
+
---
+++ signup.php
@@ -0,0 +1,29 @@
+<?php include 'header.php' ?>
+
+     
+<div class="container">
+  <h3 class="demo-panel-title">Sign Up</h3>
+  <div class="row">
+    <div class="col-xs-12 center">
+      <div class="form-group">
+        <input type="text" value="" placeholder="First Name" class="form-control" />
+      </div>
+      <div class="form-group">
+        <input type="text" value="" placeholder="Last Name" class="form-control" />
+      </div>
+      <div class="form-group">
+        <input type="text" value="" placeholder="Email" class="form-control" />
+      </div>
+      <div class="form-group">
+        <input type="text" value="" placeholder="Password" class="form-control" />
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include 'footer.php' ?>
+
+
+
+
+
